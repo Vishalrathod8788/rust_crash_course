8e429dbe4353a8f7e20a8e735733c71986e0dcac
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-12-28 14:44:21 +0530

    Add... Dependancy

---
+++ hello_cargo/target/debug/deps/hello_cargo-fe6c550d8e3a404e.d
@@ -0,0 +1,5 @@
+/home/vishal/rust_crash_course/hello_cargo/target/debug/deps/libhello_cargo-fe6c550d8e3a404e.rmeta: src/main.rs
+
+/home/vishal/rust_crash_course/hello_cargo/target/debug/deps/hello_cargo-fe6c550d8e3a404e.d: src/main.rs
+
+src/main.rs:
